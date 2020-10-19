1f71ff42dbce238ed504f707cd77dc41969d9ec6
yvelezsa <yeisson.velez@hotmail.de>
2020-10-19 16:16:48 +0200

    Added templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from polls.models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Ok")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    
+    # method 1 return HttpResponse(template.render(context, request))
+    # shortcut 
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    """  try:
+        question = Question.objects.get(pk=question_id)
+        except Question.DoesNotExist:
+        raise Http404("Question doesn't exist")
+        return render(request, 'polls/detail.html', {'question': question})
+    """ 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def addquestion(request):
+    return render(request, 'polls/add-question.html')
